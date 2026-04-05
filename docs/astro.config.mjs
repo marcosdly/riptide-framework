ca6793e63f6774e83a623dc915db85b441e97121
rep <daniilcot523@gmail.com>
2026-04-05 18:43:08 +0300

    ci: add docs deployment workflow

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://riptide-project.github.io',
+	base: '/framework',
 	integrations: [
 		starlight({
 			title: 'Riptide Framework',
